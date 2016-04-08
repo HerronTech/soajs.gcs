20d2d699b3bfebd7d4a0362294aa821a1a7da282
elianenassif <nassif.eliane@gmail.com>
2016-04-08 12:11:42 +0300

    updates to testing data

--- test/integration/_server.test.js
+++ test/integration/_server.test.js
@@ -7,29 +7,29 @@ var controller, urac;
 
 describe("importing sample data", function () {
 
-    it("do import", function (done) {
-        shell.pushd(sampleData.dir);
-        shell.exec("chmod +x " + sampleData.shell, function (code) {
-            assert.equal(code, 0);
-            shell.exec(sampleData.shell, function (code) {
-                assert.equal(code, 0);
-                shell.popd();
-                done();
-            });
-        });
-    });
+	it("do import", function (done) {
+		shell.pushd(sampleData.dir);
+		shell.exec("chmod +x " + sampleData.shell, function (code) {
+			assert.equal(code, 0);
+			shell.exec(sampleData.shell, function (code) {
+				assert.equal(code, 0);
+				shell.popd();
+				done();
+			});
+		});
+	});
 
-    after(function (done) {
-        process.env.SOAJS_ENV = "dashboard";
-        process.env.SOAJS_SRVIP = "127.0.0.1";
-        console.log('test data imported.');
-        controller = require("soajs.controller");
-        setTimeout(function () {
-            urac = require("soajs.urac");
-            setTimeout(function () {
-                require("./soajs.gc.test.js");
-                done();
-            }, 1000);
-        }, 1000);
-    });
+	after(function (done) {
+		process.env.SOAJS_ENV = "dev";
+		//process.env.SOAJS_SRVIP = "127.0.0.1";
+		console.log('test data imported.');
+		controller = require("soajs.controller");
+		setTimeout(function () {
+			urac = require("soajs.urac");
+			setTimeout(function () {
+				require("./soajs.gc.test.js");
+				done();
+			}, 1000);
+		}, 1000);
+	});
 });
